3b28a7bfa6f5929dc24aee4d4fa92c2e81527220
TheRubixCoder <akilan2603@gmail.com>
2025-08-07 22:53:33 +0530

    allowed new domain

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://tmhire-frontend.vercel.app"], 
+    allow_origins=["http://localhost:3000","https://tmhire-frontend.vercel.app", "https://tmgrid.in"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
