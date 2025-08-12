9a4f84b1aed5cc236c017bf15c4d1a896a6095c5
TheRubixCoder <akilan2603@gmail.com>
2025-08-12 19:14:46 +0530

    global format

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from pydantic import BaseModel, Field, EmailStr, ConfigDict
-from typing import Optional
+from typing import Literal, Optional
 from app.db.mongodb import PyObjectId
 from bson import ObjectId
 
@@ -13,6 +13,8 @@ class UserModel(BaseModel):
     contact: Optional[int] = Field(default=None, description="Phone number of the user")
     company: Optional[str] = Field(default=None, description="Company that the user works for")
     city: Optional[str] = Field(default=None, description="Location of the user")
+    preferred_format: Optional[Literal["12h", "24h"]] = "24h"
+    custom_start_hour: Optional[int] = 0
     created_at: datetime = Field(default_factory=datetime.utcnow)
     
     model_config = ConfigDict(
@@ -27,6 +29,8 @@ class UserModel(BaseModel):
                 "contact": 1234567890,
                 "company": "Main Concrete Firm",
                 "city": "Coimbatore",
+                "preferred_format": "24h",
+                "custom_start_hour": 0,
                 "created_at": datetime.utcnow()
             }
         }
@@ -40,16 +44,21 @@ class UserCreate(BaseModel):
     contact: Optional[int] = Field(default=None, description="Phone number of the user")
     company: Optional[str] = Field(default=None, description="Company that the user works for")
     city: Optional[str] = Field(default=None, description="Location of the user")
-    
+    preferred_format: Optional[Literal["12h", "24h"]] = "24h"
+    custom_start_hour: Optional[int] = 0
+
     model_config = ConfigDict(
         json_schema_extra={
             "example": {
                 "email": "user@example.com",
+                "password": "securepassword123",
                 "name": "John Doe",
                 "new_user": False,
                 "contact": 1234567890,
                 "company": "Main Concrete Firm",
-                "city": "Coimbatore"
+                "city": "Coimbatore",
+                "preferred_format": "24h",
+                "custom_start_hour": 0,
             }
         }
     ) 
@@ -64,5 +73,19 @@ class UserUpdate(BaseModel):
     contact: Optional[int] = None
     company: Optional[str] = None
     city: Optional[str] = None
-    
+    preferred_format: Optional[Literal["12h", "24h"]] = "24h"
+    custom_start_hour: Optional[int] = 0
 
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "name": "John Doe",
+                "password": "newpassword123",
+                "contact": 1234567890,
+                "company": "Main Concrete Firm",
+                "city": "Coimbatore",
+                "preferred_format": "24h",
+                "custom_start_hour": 0,
+            }
+        }
+    ) 
